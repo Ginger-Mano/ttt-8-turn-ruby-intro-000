993dc98afdc8d64852956a216517d45259dd8f29
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-03-02 20:51:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,7 @@
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.chomp
-  input_to_index(user_input)
+  index=input_to_index(user_input)
   if valid_move?(board, index)
     display_board(board)
   else !index.between?(0,8)
